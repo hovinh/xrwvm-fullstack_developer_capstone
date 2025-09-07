7e9186a1ab2682ffa1f9ef2b6ae369311c567dd5
hovinh <hovinh39@gmail.com>
2025-09-07 10:36:33 +0800

    new add

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,16 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
+    if kwargs:
         for key, value in kwargs.items():
             params += key + "=" + value + "&"
 
@@ -29,8 +28,9 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred")
 
+
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -41,9 +41,9 @@ def analyze_review_sentiments(text):
 
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
