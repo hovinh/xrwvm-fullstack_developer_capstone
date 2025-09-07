863dfe7ee56b44997d3a255a78fc6fcfe7cded13
hovinh <hovinh39@gmail.com>
2025-09-07 10:31:43 +0800

    add

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,6 +19,7 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
@@ -28,7 +29,6 @@ class CarMake(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -45,8 +45,8 @@ class CarModel(models.Model):
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
-    # Other fields as needed
 
     def __str__(self):
-        return self.name # Return the name as the string representation
+        return self.name
+    
     
